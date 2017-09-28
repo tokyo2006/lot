5c790fc3c195308723847d40a683e843bf72671d
Chen Zeng <zec@cn.tradeshift.com>
2017-09-28 09:48:19 +0800

    add trace line

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 
 var temp = 10;
 var humi = 50;
+var linePoints = []
 var currentUser = getCurrentUser(); 
+linePoints.push(currentUser);
+var pointsLen = linePoints.length,i,polyline;  
 var opts = {
     width : 200,     // 信息窗口宽度
     height: 100,     // 信息窗口高度
@@ -23,16 +26,31 @@ $(document).ready(function(){
     map.addOverlay(marker);   
     map.panTo(currentUser);
 
-    function show(){
+    function addLine(points){  
+        
 
+          if(pointsLen == 0){  
+              return;  
+          }  
+          // 创建标注对象并添加到地图     
+          for(i = 0;i <pointsLen;i++){  
+              linePoints.push(new BMap.Point(points[i].lng,points[i].lat));  
+          }  
+        
+          polyline = new BMap.Polyline(linePoints, {strokeColor:"red", strokeWeight:2, strokeOpacity:0.5});   //创建折线  
+          map.addOverlay(polyline);   //增加折线  
+     } 
+    function show(){
         temp = Math.random()*20+20;
         humi = Math.random()*20+50;
         map.closeInfoWindow(infoWindow,currentUser);
         currentUser = new BMap.Point(120.61990712,31+   (Math.random() * 0.007 + 0.0000015), 31.31798737 + (Math.random() * 0.007 + 0.000000015));
+        linePoints.push(currentUser);
+        addLine(linePoints);
         marker.setPosition(currentUser);       
         infoWindow = new BMap.InfoWindow('T:'+temp+'C°<br/>H:'+humi+'% <br/><input id="door" type="button" onclick="opendoor();" value="Open Door" />', opts); 
     }
-    setInterval(show,10000);
+    setInterval(show,1000);
     marker.addEventListener("click", function(){          
 		map.openInfoWindow(infoWindow,currentUser); //开启信息窗口
 	});
@@ -44,3 +62,6 @@ function opendoor(){
 function getCurrentUser(){ 
     return new BMap.Point(120.61990712,31.31798737);
 }
+
+
+
